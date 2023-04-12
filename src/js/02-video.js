54f297e0a94ebfd50af852076c76200d7d631cf7
Karina-98 <115569415+Karina-98@users.noreply.github.com>
2023-04-12 16:53:42 +0200

    +

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,29 +1,22 @@
-import { player } from "@vimeo/player";
-import { _throttle } from "lodash.throttle";
 
-    const iframe = document.querySelector("iframe");
-    const player = new Vimeo.Player(iframe);
-    const currentTimeVideo = "videoplayer-current-time"
-
-
-
-
-player.on('timeupdate', trottle(onPlay, 1000) );
-
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const currentTimeVideo = 'videoplayer-current-time';
+player.on('timeupdate', throttle(onPlay, 1000));
 function onPlay(event) {
-    lokalStorege.setItem(currentTimeVideo, event.seconds);
-
+  localStorage.setItem(currentTimeVideo, event.seconds);
 }
-setCurrentTime();
+player.setCurrentTime(localStorage.getItem(currentTimeVideo) || 0);
 
-// function setCurrentTime() { 
-//     if (!localStorage.getItem(currentTimeVideo)){
-//         return;
-// }
-//     player.setCurrentTime(localStorage.getItem('currentTimeVideo'));
-// }
 
-// player.setCurrentTime(localStorage.getItem(currentTimeVideo) || 0);
+
+
+
+
+
+
 
 
 
